1b9044dfccd32f451e1cec374e534ff77510f8ce
chen <chenzabatani@gmail.com>
2026-01-25 23:21:37 +0200

    Refactor: Implement Generic BaseController for Posts and Comments

--- src/controllers/comment.ts
+++ src/controllers/comment.ts
@@ -1,57 +1,15 @@
-import Comment from '../models/comment_model';
 import { Request, Response } from 'express';
+import CommentModel, { IComment } from '../models/comment_model';
+import { BaseController } from './base_controller';
 
-const createComment = async (req: Request, res: Response) => {
-    try {
-        const comment = await Comment.create(req.body);
-        res.status(201).json(comment);
-    } catch (error) {
-        res.status(400).json({ message: (error as Error).message });
+class CommentController extends BaseController<IComment> {
+    constructor() {
+        super(CommentModel);
     }
-};
 
-const getAllComments = async (req: Request, res: Response) => {
-    const filter = req.query.postId ? { postId: req.query.postId } : {};
-
-    try {
-        const comments = await Comment.find(filter);
-        res.send(comments);
-    } catch (error) {
-        res.status(400).json({ message: (error as Error).message });
+    async create(req: Request, res: Response) {
+        return super.create(req, res);
     }
-};
+}
 
-const getCommentById = async (req: Request, res: Response) => {
-    try {
-        const comment = await Comment.findById(req.params.id);
-        res.send(comment);
-    } catch (error) {
-        res.status(400).json({ message: (error as Error).message });
-    }
-};
-
-const updateComment = async (req: Request, res: Response) => {
-    try {
-        const comment = await Comment.findByIdAndUpdate(req.params.id, req.body, { new: true });
-        res.send(comment);
-    } catch (error) {
-        res.status(400).json({ message: (error as Error).message });
-    }
-};
-
-const deleteComment = async (req: Request, res: Response) => {
-    try {
-        await Comment.findByIdAndDelete(req.params.id);
-        res.send({ message: "Deleted" });
-    } catch (error) {
-        res.status(400).json({ message: (error as Error).message });
-    }
-};
-
-export default {
-    createComment,
-    getAllComments,
-    getCommentById,
-    updateComment,
-    deleteComment
-};
+export default new CommentController();
--- src/routes/post_routes.ts
+++ src/routes/post_routes.ts
@@ -3,9 +3,10 @@ import postController from '../controllers/post';
 
 const router = express.Router();
 
-router.post('/', postController.createPost);
-router.get('/', postController.getAllPosts);
-router.get('/:id', postController.getPostById);
-router.put('/:id', postController.updatePost);
+router.post('/', postController.create.bind(postController));
+router.get('/', postController.getAll.bind(postController));
+router.get('/:id', postController.getById.bind(postController));
+router.put('/:id', postController.update.bind(postController));
+router.delete('/:id', postController.delete.bind(postController));
 
 export default router;
