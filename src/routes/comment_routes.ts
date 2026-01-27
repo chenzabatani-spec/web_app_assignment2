c4ae2fa06e53c7191a5e4338e2a843d8c3ba4d48
chen <chenzabatani@gmail.com>
2026-01-27 20:43:46 +0200

    Adjusted  comments API with Auth and Post relationship

--- src/routes/comment_routes.ts
+++ src/routes/comment_routes.ts
@@ -1,12 +1,16 @@
 import express from 'express';
 import commentController from '../controllers/comment';
+import authMiddleware from '../middleware/auth_middleware';
 
 const router = express.Router();
 
-router.post('/', commentController.create.bind(commentController));
+// Public routes
 router.get('/', commentController.getAll.bind(commentController));
 router.get('/:id', commentController.getById.bind(commentController));
-router.put('/:id', commentController.update.bind(commentController));
-router.delete('/:id', commentController.delete.bind(commentController));
+
+// Protected routes
+router.post('/', authMiddleware, commentController.create.bind(commentController));
+router.put('/:id', authMiddleware, commentController.update.bind(commentController));
+router.delete('/:id', authMiddleware, commentController.delete.bind(commentController));
 
 export default router;
